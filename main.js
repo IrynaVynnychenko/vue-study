ea845882bd35c9146e43997a6025082d72456ee5
Iryna Vynnychenko <i.vynnychenko@gmail.com>
2021-06-28 21:28:50 +0300

    add 2 steps

---
+++ main.js
@@ -0,0 +1,11 @@
+var product = "Socks";
+var app = new Vue ({
+  el: '#app',
+  data: {
+    product: "Socks",
+    description: "A pair of warm, fuzzy socks",
+    image: "./images/socks-white.jpeg",
+    altText: "A pair of socks",
+    linkToShop: "https://rozetka.com.ua/the_pair_of_socks_roz6400000454/p208142947/"
+  }
+});
